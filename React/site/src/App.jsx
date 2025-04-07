aa95bc08fc269cf37751e84f59ca7af4f234a834
thulio31 <163910873+thulio31@users.noreply.github.com>
2025-04-06 22:45:45 -0300

    menu

--- React/site/src/App.jsx
+++ React/site/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router'; 
 import Navbar from './Components/Navbar'
+import Home from './Routes/Home';
 
 function App() {
   return (
     <>
+      
+      <BrowserRouter>
       <Navbar/>
+        <Routes>          
+            <Route path="/" element={<Navbar/>} Component={Home}  />          
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- React/site/src/Components/Navbar.jsx
+++ React/site/src/Components/Navbar.jsx
@@ -1,29 +1,49 @@
-import React from 'react'
+import React from "react";
 
-import { Link, useLocation } from 'react-router';  
-import './Navbar.css'
+import { Link } from "react-router";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <div className="main-menu">
       <header>
-        <div className="logo">
-            <Link to="/"> <img src="" alt="" /> </Link>       
+        <div className="logo">          
+          <Link to="/">
+            <img src="caminho/da/imagem.png" alt="Logo" />
+          </Link>
         </div>
-
         <nav>
           <ul>
-            <Link to="/"><li>Home</li></Link>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/sobre">Sobre</Link>
+            </li>
+            <li>
+              <Link to="/contate-nos">Contate-nos</Link>
+            </li>
+            <li>
+              <Link to="/empresa">Empresa</Link>
+            </li>
           </ul>
         </nav>
-        
         <div className="buttons">
-          <button>Login</button> 
+          <button>Login</button>
+        </div>
+        <div className="icon-bars">
+          <li><i class="fa-solid fa-bars"></i>
+            <div className="reponsive-menu">
+              <li>Home</li>
+              <li>Sobre</li>
+              <li>Contate-nos</li>
+              <li>Empresa</li>
+              <button>Login</button>
+            </div></li>
         </div>
       </header>
     </div>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
