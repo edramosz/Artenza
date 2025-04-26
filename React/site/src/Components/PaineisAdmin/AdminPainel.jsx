998ebb419f540c0cb154799e466f9ccf1828db75
thulio31 <163910873+thulio31@users.noreply.github.com>
2025-04-25 23:59:20 -0300

    ss

--- React/site/src/Components/PaineisAdmin/AdminPainel.jsx
+++ React/site/src/Components/PaineisAdmin/AdminPainel.jsx
@@ -3,59 +3,67 @@ import './AdminPainel.css';
 import AdminProduto from "./AdminProduto";
 
 const AdminPainel = () => {
-
-  const [selecao, setSelecao] = useState('painel'); //criando a seleçao para alterar os estados
-  const [sidebarAberta, setSidebarAberta] = useState(false);
-
+  const [selecao, setSelecao] = useState('painel');
 
   function renderConteudo() {
-    if (selecao === 'usuarios') {
-      return (
-        <div>
-          <h3>Usuários</h3>
-          
-        </div>
-      );
-    } else if (selecao === 'enderecos') {
-      return (
-        <div>
-          <h3>Endereços</h3>
-
-        </div>
-      );
-    } else if (selecao === 'produtos') {
-
-      return <AdminProduto />
-
+    switch (selecao) {
+      case 'usuarios':
+        return <div><h3>Usuários</h3></div>;
+      case 'enderecos':
+        return <div><h3>Endereços</h3></div>;
+      case 'produtos':
+        return <AdminProduto />;
+      default:
+        return (
+          <div className="content">
+            <h2 className="title">Visão geral do site</h2>
+            <div className="cards">
+              <div className="info-card">
+                <span className="icon"><i className="fa-solid fa-users"></i></span>
+                <div>
+                  <div className="value">203</div>
+                  <div className="label">Usuários</div>
+                </div>
+              </div>
+              <div className="info-card">
+                <span className="icon"><i className="fa-solid fa-map-location-dot"></i></span>
+                <div>
+                  <div className="value">12</div>
+                  <div className="label">Endereços</div>
+                </div>
+              </div>
+              <div className="info-card">
+                <span className="icon"><i className="fa-solid fa-cart-shopping"></i></span>
+                <div>
+                  <div className="value">33</div>
+                  <div className="label">Produtos</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
     }
-
-
-    // conteúdo Painel de Admin
-    return (
-      <div className="content">
-        <h2 className="title">Visão geral do site</h2>
-        <div className="cards">
-          <div className="info-card"><span className="icon"><i class="fa-solid fa-users"></i></span><div><div className="value">203</div><div className="label">Usuários</div></div></div>
-          <div className="info-card"><span className="icon"><i class="fa-solid fa-map-location-dot"></i></span><div><div className="value">12</div><div className="label">Endereços</div></div></div>
-          <div className="info-card"><span className="icon"><i class="fa-solid fa-cart-shopping"></i></span><div><div className="value">33</div><div className="label">Produtos</div></div></div>
-        </div>
-      </div>
-    );
   }
 
   return (
     <div className="container-dashboard">
       <aside className="sidebar">
         <h2 className="title">Painel Admin</h2>
-        <ul className="Pages">
-          <li><button onClick={() => setSelecao('painel')}>Painel de Admin</button></li>
-          <li><button onClick={() => setSelecao('usuarios')}>Usuários</button></li>
-          <li><button onClick={() => setSelecao('enderecos')}>Endereços</button></li>
-          <li><button onClick={() => setSelecao('produtos')}>Produtos</button></li>
+        <ul className="pages">
+          <li className={selecao === 'painel' ? 'active' : ''}>
+            <button onClick={() => setSelecao('painel')}><i class="fa-solid fa-gear"></i>Administração</button>
+          </li>
+          <li className={selecao === 'usuarios' ? 'active' : ''}>
+            <button onClick={() => setSelecao('usuarios')}><i className="fa-solid fa-users"></i>Usuários</button>
+          </li>
+          <li className={selecao === 'enderecos' ? 'active' : ''}>
+            <button onClick={() => setSelecao('enderecos')}><i class="fa-solid fa-location-dot"></i>Endereços</button>
+          </li>
+          <li className={selecao === 'produtos' ? 'active' : ''}>
+            <button onClick={() => setSelecao('produtos')}><i class="fa-solid fa-cart-shopping"></i>Produtos</button>
+          </li>
         </ul>
       </aside>
-
-
       <main className="render">
         {renderConteudo()}
       </main>
