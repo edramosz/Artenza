e3238d9d2e887880ff5c768b19aa41c25b68f1d9
thulio31 <163910873+thulio31@users.noreply.github.com>
2025-05-11 17:57:31 -0300

    sss

--- React/site/src/Routes/Home/Home.jsx
+++ React/site/src/Routes/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../../Components/Banner/Banner.jsx'
 import PromotionalBanner from '../../Components/Banner/PromotionalBanner.jsx'
+import Categories from '../../Components/Banner/Categories.jsx'
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <PromotionalBanner />
+      <Categories />
     </div>
   )
 }
