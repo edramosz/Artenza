dceea72fa1e3d8f4d3a3fd91a14cbd07c29dfda5
edramosz <edsonramosr726@gmail.com>
2025-05-14 17:09:03 -0300

    c

--- Core/Models/ItemVenda.cs
+++ Core/Models/ItemVenda.cs
@@ -9,7 +9,6 @@ namespace Core.Models
     public class ItemVenda
     {
         public string Id { get; set; }
-        public string VendaId { get; set; }
         public string ProdutoId { get; set; }
         public int Quantidade { get; set; }
         public decimal PrecoUnitario { get; set; }
