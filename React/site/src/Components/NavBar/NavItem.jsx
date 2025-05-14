f3f1843ffd12a66c03bc31b5b9f45759f746f3ca
thulio31 <163910873+thulio31@users.noreply.github.com>
2025-05-14 20:50:25 -0300

    sub menu
    nav bar com sub menu mais banner nao finciona por causa dela

--- React/site/src/Components/NavBar/NavItem.jsx
+++ React/site/src/Components/NavBar/NavItem.jsx
@@ -1,14 +1,36 @@
-import React from "react";
-import { Link } from "react-router";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const NavItem = (props) => {
+const NavItem = ({ url, label, IsActive, submenu }) => {
+  const [showSubmenu, setShowSubmenu] = useState(false);
+
   return (
-    <>
-      <li>
-        <Link to={props.url} className={`nav-item ${props.IsActive ? 'active' : ''}`}> {props.label} </Link>
-      </li>
-    </>
+    <li
+      className="nav-item-wrapper"
+      onMouseEnter={() => setShowSubmenu(true)}
+      onMouseLeave={() => setShowSubmenu(false)}
+    >
+      <Link to={url} className={`nav-item ${IsActive ? "active" : ""}`}>
+        {label}
+      </Link>
+
+      {submenu && showSubmenu && (
+        <ul className="submenu">
+          {submenu.map((category, index) => (
+            <li key={index} className="submenu-column">
+              <p className="submenu-title">{category.title}</p>
+              {category.items.map((item, idx) => (
+                <Link key={idx} to={item.url} className="submenu-item">
+                  {item.label}
+                </Link>
+              ))}
+            </li>
+          ))}
+        </ul>
+      )}
+    </li>
   );
 };
+
 
 export default NavItem;
