d8b28cbd2de7a90307e79f788b558c4673b18e51
edramosz <edsonramosr726@gmail.com>
2025-05-23 13:47:54 -0300

    correção do nomes de class

--- Core/Models/Usuario.cs
+++ Core/Models/Usuario.cs
@@ -13,7 +13,6 @@ namespace Core.Models
         public int DiaNascimento { get; set; }
         public int MesNascimento { get; set; }
         public int AnoNascimento { get; set; }
-        public string SenhaHash { get; set; }
         public string PerfilUrl { get; set; }
         public bool isAdmin { get; set; }
         public DateTime DataCadastro { get; set; }
