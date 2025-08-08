4b45b764c11da12199ea29dad5a4e6fbf632bea0
edramosz <edsonramosr726@gmail.com>
2025-08-08 16:43:20 -0300

    crud de cartao implementado

---
+++ Core/Interfaces/ICartaoService.cs
@@ -0,0 +1,21 @@
+﻿using Core.Models.DTO_s.Create;
+using Core.Models.DTO_s.Update;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface ICartaoService
+    {
+        Task<List<Cartao>> GetCartoesAsync();
+        Task<Cartao> GetCartaoAsync(string id);
+        Task<List<Cartao>> GetCartaoPorIdUsuario(string idUsuario);
+        Task<Cartao> AddCartaoAsync(CreateCartao cartaoDto);
+        Task UpdateCartaoAsync(string id, UpdateCartao cartao);
+        Task DeleteCartaoAsync(string id);
+    }
+}
---
+++ Core/Models/Cartao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Cartao
+    {
+        public string Id { get; set; }
+        public string UsuarioId { get; set; }
+        public string NumeroCartao { get; set; }
+        public string NomeTitular { get; set; }
+        public DateTime Validade { get; set; }
+        public string CVV { get; set; }
+    }
+}
